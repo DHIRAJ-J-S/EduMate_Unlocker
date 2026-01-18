f2e7ad3ef456ade7a2b50fc354ec272d64da44d1
Dhiraj J S <77600726+DHIRAJ-J-S@users.noreply.github.com>
2026-01-18 17:33:16 +0530

    Update MainActivity.kt

--- app/src/main/java/com/the404guy/em_unlocker/MainActivity.kt
+++ app/src/main/java/com/the404guy/em_unlocker/MainActivity.kt
@@ -732,7 +732,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
-            R.id.action_settings -> {
+            R.id.menu_settings -> {
                 showSettingsDialog()
                 true
             }
